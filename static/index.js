6c6bab658c86424bf8fe758a904a613e9aca1809
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-04-23 18:54:34 -0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -2,38 +2,35 @@
 /**
  * @type {HTMLFormElement}
  */
-const form = document.getElementById("form");
+const form = document.getElementById("uv-form");
 /**
  * @type {HTMLInputElement}
  */
-const address = document.getElementById("address");
+const address = document.getElementById("uv-address");
 /**
  * @type {HTMLInputElement}
  */
-const searchEngine = document.getElementById("search-engine");
-
-function help() {
-    document.getElementById('address').value = __protocoller$config.protocol+__protocoller$config.suffix+__protocoller$config.helpURI;
-    test()
-}
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
 
 form.addEventListener("submit", async (event) => {
-    event.preventDefault()
-    var minEng = address.value.replace("https://www.google.com/search?q=%s", '')
-    console.log('address.value:', address.value);
-    console.log('minEng:', minEng);
-    if (address.value.includes(__protocoller$config.protocol+__protocoller$config.suffix)) {
-        test()
-    } else {
-        try {
-            await registerSW();
-        } catch (err) {
-            alert("Failed to register service worker.\n"+err.toString()+"|<button onclick=\"location.reload()\">return</button>")
-            throw err;
-        }
+  event.preventDefault();
 
-        const url = search(address.value, searchEngine.value);
-        sessionStorage.setItem("ur", __uv$config.prefix + __uv$config.encodeUrl(url))
-        location.href = "go.html"
-    }
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
