e1bc8881c92bc557bef5fe5c2a7240e41df36b9a
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-04-23 19:13:56 -0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -11,29 +11,27 @@ const address = document.getElementById("uv-address");
  * @type {HTMLInputElement}
  */
 const searchEngine = document.getElementById("uv-search-engine");
-
-function help() {
-    document.getElementById('address').value = __protocoller$config.protocol+__protocoller$config.suffix+__protocoller$config.helpURI;
-    test()
-}
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
 
 form.addEventListener("submit", async (event) => {
-    event.preventDefault()
-    var minEng = address.value.replace("https://www.google.com/search?q=%s", '')
-    console.log('address.value:', address.value);
-    console.log('minEng:', minEng);
-    if (address.value.includes(__protocoller$config.protocol+__protocoller$config.suffix)) {
-        test()
-    } else {
-        try {
-            await registerSW();
-        } catch (err) {
-            alert("Failed to register service worker.\n"+err.toString()+"|<button onclick=\"location.reload()\">return</button>")
-            throw err;
-        }
+  event.preventDefault();
 
-        const url = search(address.value, searchEngine.value);
-        sessionStorage.setItem("ur", __uv$config.prefix + __uv$config.encodeUrl(url))
-        location.href = "go.html"
-    }
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+   location.href = "go.html"
 });
