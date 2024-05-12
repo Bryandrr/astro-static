8088165f6a751cf54df3d1143d58580d6a2b60bf
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-12 11:18:34 -0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -32,5 +32,40 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  function abtblank(url) {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  let inFrame;
+
+  try {
+    inFrame = window !== top;
+  } catch (e) {
+    inFrame = true;
+  }
+
+  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = window.open("about:blank", name, `width=${width},height=${height}`);
+
+    if (!popup || popup.closed) {
+      alert("Allow popups and redirects to hide this from showing up in your history.");
+    } else {
+      const doc = popup.document;
+      const iframe = doc.createElement("iframe");
+      const style = iframe.style;
+      const link = doc.createElement("link");
+
+
+      iframe.src = url;
+      style.position = "fixed";
+      style.top = style.bottom = style.left = style.right = 0;
+      style.border = style.outline = "none";
+      style.width = style.height = "100%";
+
+      doc.head.appendChild(link);
+      doc.body.appendChild(iframe);
+      window.location.replace("https://google.com");
+    }
+  }
+}
 });
