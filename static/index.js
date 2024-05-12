9be34f52b6825bb1fa04e74ec22f1708c4467b86
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-12 12:54:15 -0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,9 +1,36 @@
-const form = document.getElementsByClassName('uv-form')[0];
-const input = document.getElementsByClassName('uv-address')[0];
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
 
-if (form && input) {
-    let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-    openNewTab(window.location.origin.concat(__uv$config.prefix + __uv$config.encodeUrl(url)));
-}
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
