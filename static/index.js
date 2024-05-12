1242c88adf68830aacdced1d202c779155d4a5b4
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-12 12:50:34 -0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -2,13 +2,8 @@ const form = document.getElementsByClassName('uv-form')[0];
 const input = document.getElementsByClassName('uv-address')[0];
 
 if (form && input) {
-    form.addEventListener('submit', async event => {
-        event.preventDefault();
-        let url = input.value.trim();
+    let url = input.value.trim();
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-
-        window.alert('If it does not work the first time click again!')
-        openNewTab(window.location.origin.concat(__uv$config.prefix + __uv$config.encodeUrl(url)));
-    });
+    openNewTab(window.location.origin.concat(__uv$config.prefix + __uv$config.encodeUrl(url)));
 }
