acd6ebdb1f162258c880657d6176bb7db8d8a67d
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-12 16:51:04 -0700

    Update uv.js

--- static/uv.js
+++ static/uv.js
@@ -1,7 +1,8 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
+importScripts("/assets/uv/uv.bundle.js");
+importScripts("/assets/uv/uv.config.js");
+importScripts("/assets/uv/uv.sw.js");
 
 const sw = new UVServiceWorker();
+let userKey = new URL(location).searchParams.get("userkey");
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
