9c52c4a4ff2a12f9a33d7b42923d9a6ee6f8203c
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-12 11:32:56 -0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,24 +1,36 @@
-const form = document.getElementsByClassName('uv-form')[0];
-const input = document.getElementsByClassName('form__input')[0];
-var section = document.getElementById('section');
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
 
-if (form && input) {
-    form.addEventListener('submit', async event => {
-        event.preventDefault();
-        window.navigator.serviceWorker.register('./sw.js', {
-            scope: __uv$config.prefix
-        }).then(() => {
-            let url = input.value.trim();
-            if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-            else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-            window.alert('If it does not work the first time click again!')
-            openNewTab(window.location.origin.concat(__uv$config.prefix + __uv$config.encodeUrl(url)));
-        });
-    });
-}
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
 
-function isUrl(val = '') {
-    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
-    return false;
-};
+  const url = search(address.value, searchEngine.value);
+location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
